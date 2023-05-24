a122686a67f63af2794f099a6622c393f801cf7c
navam <navam@DESKTOP-UMEOD27>
2023-05-24 17:48:53 +0300

    Adding files and functions

---
+++ HashTables/HSubject.cpp
@@ -0,0 +1,24 @@
+#include "HashTable.h"
+#include "HSubject.h"
+
+using namespace std;
+
+void HSubject::printS(string ky) {
+
+}
+
+void HSubject::printN(string ky, int N) {
+
+}
+
+void HSubject::print() {
+
+}
+
+void HSubject::startNewTable() {
+
+}
+
+void HSubject::addSubjectAndTitle(string s, string t) {
+
+}
